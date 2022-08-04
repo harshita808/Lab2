76eca8937cb1aadfc6ed7203d5339bd8453d8cb7
harshita <64593306+harshita808@users.noreply.github.com>
2022-08-04 14:28:15 +0530

    update sol

--- contracts/Trade.sol
+++ contracts/Trade.sol
@@ -1,23 +1,8 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.5.0 <0.9.0;
-contract Trade{
-    address  payable public owner;
-    address public buyer;
-    uint public price;
 
-    constructor() {
-        owner=payable(msg.sender);
-    }
-
-    
-    function setPrice(uint _price) public {
-        require(msg.sender==owner,"Only owner can call this function");
-        price=_price;
-        
-    }
-   
-   
-   
+//Few lines of code are given for your help, complete the rest. You can do it :)
+contract Trade{  
 
  function pay()external payable{
     require(msg.value==price,"value not same");
@@ -26,16 +11,6 @@ contract Trade{
     buyer=msg.sender;
    
  }
-
-function getBalance() public view returns (uint){
-    require(msg.sender==owner,"Only owner can call this function");
-        return address(this).balance;
-    }
-   
-   function withdraw() public payable  {
-        require(msg.sender==owner,"Only owner can call this function");
-        owner.transfer(address(this).balance);
-    }
-
+ 
 }
     
