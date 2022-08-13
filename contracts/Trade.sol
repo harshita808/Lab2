ca87ba414c7ee33fc90559c9478b2dd8c17df6ae
harshita <64593306+harshita808@users.noreply.github.com>
2022-08-13 15:36:19 +0530

    update sol

--- contracts/Trade.sol
+++ contracts/Trade.sol
@@ -1,40 +1,6 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.5.0 <0.9.0;
 contract Trade{
-    address  payable public owner;
-    address public buyer;
-    uint public price;
-
-    constructor() {
-        owner=payable(msg.sender);
-    }
-
     
-    function setPrice(uint _price) public {
-        require(msg.sender==owner,"Only owner can call this function");
-        price=_price;
-        
-    }
-   
-   
-   
-
- function pay()external payable{
-    require(msg.value==price,"value not same");
-    uint a=address(this).balance;
-    a+=msg.value;
-    buyer=msg.sender;
-   
- }
-
-function getBalance() public view returns (uint){
-    require(msg.sender==owner,"Only owner can call this function");
-        return address(this).balance;
-    }
-   
-   function withdraw() public payable  {
-        require(msg.sender==owner,"Only owner can call this function");
-        owner.transfer(address(this).balance);
-    }
-
+      //Write your code here
 }
